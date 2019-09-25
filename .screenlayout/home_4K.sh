747be24fb9cc94042382d98d30522d4900a5842c
daan <dw.daanwijns@gmail.com>
2019-09-25 20:41:07 +0200

    added arandr layouts

---
+++ .screenlayout/home_4K.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP1 --primary --mode 1920x1080 --pos 770x2160 --rotate normal --output DP1 --mode 3840x2160 --pos 0x0 --rotate normal --output DP2 --off --output HDMI1 --off --output HDMI2 --off --output VIRTUAL1 --off
