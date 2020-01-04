d345aa4e294329208b29e93e9770bd022f0cfee0
Daan Wijns <dw.daanwijns@gmail.com>
2020-01-04 20:30:31 +0100

    rewrite with selection menu

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -11,9 +11,6 @@ update_pi(){
 
     #clean useless packages
     sudo apt autoremove
-    
-    #add general packages
-    sudo apt-get install git curl tree trash-cli ufw neofetch
 }
 
 update_deb(){
@@ -26,26 +23,34 @@ update_deb(){
 	#clean
 	sudo apt autoclean
 	sudo apt autoremove -y
-	
-	#add general packages
-    	sudo apt-get install git curl tree trash-cli ufw neofetch
 }
 
 update_arch(){
 	sudo pacman -Syu
 
 	#clean
-	sudo pacman -Sc
+	sudo pacman -Scc
+}
+
+#general packages
+general_packages(){
+	if [ $debian = 'true' ]
+	then
+        sudo apt-get install git curl tree trash-cli neofetch
+	elif [ $arch = 'true' ]
+	then
+	sudo pacman -S git curl tree trash-cli neofetch
+	fi
 }
 
 install_zsh(){
+  	if [ $debian = 'true' ] || [ $rpi = 'true' ]
+	then
     sudo apt install zsh 
-}
-
-change_hostname(){
-    sudo rm /etc/hostname
-    sudo touch /etc/hostname
-    echo $hostname | sudo tee -a /etc/hostname
+	elif [ $arch = 'true' ]
+	then
+	sudo pacman -S zsh
+	fi
 }
 
 install_node(){
@@ -59,10 +64,33 @@ install_rmate(){
     sudo chmod a+x /usr/local/bin/rsub
 }
 
+instal_docker(){
+	if [ $debian = 'true' ]
+	then
+    sudo apt install docker docker-compose -y 
+	elif [ $arch = 'true' ]
+	then
+	sudo pacman -S docker docker-compose
+	elif [ $rpi = 'true' ]
+	then
+	curl -fsSL get.docker.com -o get-docker.sh && sh get-docker.sh
+	fi
+
+	#adding user to docker
+	sudo usermod -aG docker $username
+}
+
 install_python(){
+    if [ $debian = 'true' ] || [ $rpi = 'true' ]
+	then
     sudo apt-get install python3-pip -y
     sudo apt-get install python3 -y
     sudo apt-get install python3-pygments
+	elif [ $arch = 'true' ]
+	then
+	sudo pacman -S python3 python3-pip pygmentize
+	fi
+    
 }
 
 configure_zsh(){
@@ -75,11 +103,9 @@ configure_zsh(){
     rm .zshrc
     cd ~
 
-cat <<EOT >> .zshrc
+cat <<EOT >> ~/.zshrc
 export ZSH="/home/${username}/.oh-my-zsh"
-
 ZSH_THEME="agnoster"
-
 plugins=(
     git
     zsh-syntax-highlighting
@@ -89,75 +115,172 @@ plugins=(
     npm
     git
 )
-
-
 source $ZSH/oh-my-zsh.sh
-
-
 alias start_docker='sudo systemctl start docker'
 alias stop_docker='docker kill $(docker ps -q) && sleep 1  &&  sudo systemctl stop docker'
 alias dc='sudo docker-compose -f /home/${username}/Documents/docker-compose.yml up -d --remove-orphans'
 alias plz='sudo $(fc -ln -1)'
 alias cat='pygmentize -g'
 alias usage='du -h -d1'
-
-
 ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=5'
 EOT
 
 
-    source .zshrc
+    source ~/.zshrc
 }
 
 adding_alias(){
-    echo "alias supdate='sudo apt-get update && sudo apt-get upgrade -y'" >> .zshrc
-    source .zshrYc
+    echo "alias supdate='sudo apt-get update && sudo apt-get upgrade -y'" >> ~/.zshrc
+    source ~/.zshrc
 }
 
-read -p 'using a rpi? y/n:  ' rpi
+change_hostname(){
+    sudo rm /etc/hostname
+    sudo touch /etc/hostname
+    echo $hostname | sudo tee -a /etc/hostname
+}
 
-if [ $rpi = 'y' ]
-then
-	echo update_pi
-else
-	read -p 'using a debian base? y/n (arch):  ' distro
-	if [ $distro = 'y' ]
-	then
-		update_deb
-	else
-		update_arch
+start_install(){
+	if [ $result[0] = 'true' ]
+	 then 
+	 	install_zsh
+	  	configure_zsh
 	fi
-fi
+	if [ $result[1] = 'true' ]; then install_node; fi;
+	if [ $result[2] = 'true' ]; then install_rmate; fi;
+	if [ $result[3] = 'true' ]; then install_python; fi;
+	if [ $result[4] = 'true' ]; then instal_docker; fi;
+}
 
-read -p 'which hostname?:  ' hostname
-read -p 'which username?:  ' username
+function multiselect {
 
-change_hostname hostname
+    # little helpers for terminal print control and key input
+    ESC=$( printf "\033")
+    cursor_blink_on()   { printf "$ESC[?25h"; }
+    cursor_blink_off()  { printf "$ESC[?25l"; }
+    cursor_to()         { printf "$ESC[$1;${2:-1}H"; }
+    print_inactive()    { printf "$2   $1 "; }
+    print_active()      { printf "$2  $ESC[7m $1 $ESC[27m"; }
+    get_cursor_row()    { IFS=';' read -sdR -p $'\E[6n' ROW COL; echo ${ROW#*[}; }
+    key_input()         {
+      local key
+      IFS= read -rsn1 key 2>/dev/null >&2
+      if [[ $key = ""      ]]; then echo enter; fi;
+      if [[ $key = $'\x20' ]]; then echo space; fi;
+      if [[ $key = $'\x1b' ]]; then
+        read -rsn2 key
+        if [[ $key = [A ]]; then echo up;    fi;
+        if [[ $key = [B ]]; then echo down;  fi;
+      fi 
+    }
+    toggle_option()    {
+      local arr_name=$1
+      eval "local arr=(\"\${${arr_name}[@]}\")"
+      local option=$2
+      if [[ ${arr[option]} == true ]]; then
+        arr[option]=
+      else
+        arr[option]=true
+      fi
+      eval $arr_name='("${arr[@]}")'
+    }
 
-if [ $rpi = 'y' ] || [ $distro = 'y' ]
-then
-install_zsh
-install_node
-install_rmate
-install_python
-else
-    sudo pacman -S git base-devel
-	sudo pacman -S zsh
-    sudo pacman -S nodejs npm python-pygments tree
-    sudo pacman -S docker docker-compose
-    cd ~
-    #install yay
-    git clone https://aur.archlinux.org/yay.git
-    cd yay
-    makepkg -si
-fi
+    local retval=$1
+    local options
+    local defaults
 
-configure_zsh
+    IFS=';' read -r -a options <<< "$2"
+    if [[ -z $3 ]]; then
+      defaults=()
+    else
+      IFS=';' read -r -a defaults <<< "$3"
+    fi
+    local selected=()
 
+    for ((i=0; i<${#options[@]}; i++)); do
+      selected+=("${defaults[i]}")
+      printf "\n"
+    done
 
-if [ $rpi = 'y' ] || [ $distro = 'y' ]
-then
-    adding_alias
-fi
+    # determine current screen position for overwriting the options
+    local lastrow=`get_cursor_row`
+    local startrow=$(($lastrow - ${#options[@]}))
 
-echo 'DONE.'
+    # ensure cursor and input echoing back on upon a ctrl+c during read -s
+    trap "cursor_blink_on; stty echo; printf '\n'; exit" 2
+    cursor_blink_off
+
+    local active=0
+    while true; do
+        # print options by overwriting the last lines
+        local idx=0
+        for option in "${options[@]}"; do
+            local prefix="[ ]"
+            if [[ ${selected[idx]} == true ]]; then
+              prefix="[x]"
+            fi
+
+            cursor_to $(($startrow + $idx))
+            if [ $idx -eq $active ]; then
+                print_active "$option" "$prefix"
+            else
+                print_inactive "$option" "$prefix"
+            fi
+            ((idx++))
+        done
+
+        # user key control
+        case `key_input` in
+            space)  toggle_option selected $active;;
+            enter)  break;;
+            up)     ((active--));
+                    if [ $active -lt 0 ]; then active=$((${#options[@]} - 1)); fi;;
+            down)   ((active++));
+                    if [ $active -ge ${#options[@]} ]; then active=0; fi;;
+        esac
+    done
+
+    # cursor position back to normal
+    cursor_to $lastrow
+    printf "\n"
+    cursor_blink_on
+
+    eval $retval='("${selected[@]}")'
+}
+
+#asking which packages
+multiselect result "oh-my-zsh;node;rmate;python;docker" "true;true;;true;;"
+
+read -p 'which hostname?  ' hostname
+read -p 'which username?  ' username
+
+change_hostname
+
+#asking which kind of system
+PS3='Please enter your choice: '
+options=("rpi" "debian" "arch" "Quit")
+select opt in "${options[@]}"
+do
+	case $opt in
+        "rpi")
+		  rpi='true'
+		  start_install
+		  adding_alias
+          break
+          ;;
+        "debian")
+		  debian='true'
+		  start_install
+		  adding_alias
+          break
+          ;;
+        "arch")
+		  arch='true'
+		  start_install
+          break
+          ;;
+        *) echo "invalid option";;
+	esac
+done
+
+echo 'DONE!!!'
