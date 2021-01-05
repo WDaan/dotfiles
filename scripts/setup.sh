5cb35d5b471a7b61dbad08bcc042f16c62fe09e8
Daan Wijns <dw.daanwijns@gmail.com>
2021-01-05 12:32:44 +0000

    setup script user feedback & check if allready installed

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,6 +2,7 @@
 
 # update package and firmware
 update() {
+  echo -e "\033[36mUpdating System...\e[0m"
   sudo apt update && sudo apt upgrade -y
   sudo apt-get dist-upgrade -y
 
@@ -17,74 +18,130 @@ general_packages() {
 }
 
 install_node() {
-  echo -e "\033[36Installing Node..\e[0m"
-  curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
-  sudo apt install -y nodejs
-  # seperate because npm needs nodejs before it can install
-  sudo apt install npm -y
+  if which node >/dev/null; then
+    echo -e "\033[32m✔ Node is installed \e[0m"
+  else
+    echo -e "\033[31m❌ Node missing\e[0m"
+    echo -e "\033[36Installing now...\e[0m"
+    curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
+    sudo apt install -y nodejs
+    # seperate because npm needs nodejs before it can install
+    sudo apt install npm -y
 
-  mkdir "${HOME}/.config/npm" || (mkdir "${HOME}/.config" && mkdir "${HOME}/.config/npm") || true
-  npm config set prefix "${HOME}/.config/npm"
+    mkdir "${HOME}/.config/npm" || (mkdir "${HOME}/.config" && mkdir "${HOME}/.config/npm") || true
+    npm config set prefix "${HOME}/.config/npm"
+  fi
 }
 
 install_rmate() {
-  echo -e "\033[36Installing rmate..\e[0m"
-  sudo wget -O /usr/local/bin/rsub \https://raw.github.com/aurora/rmate/master/rmate
-  sudo chmod a+x /usr/local/bin/rsub
+  if which rsub >/dev/null; then
+    echo -e "\033[32m✔ rmate is installed\e[0m"
+  else
+    echo -e "\033[31m❌ rmate missing\e[0m"
+    echo -e "\033[36Installing now...\e[0m"
+    sudo wget -O /usr/local/bin/rsub \https://raw.github.com/aurora/rmate/master/rmate
+    sudo chmod a+x /usr/local/bin/rsub
+  fi
 }
 
 instal_docker() {
-  echo -e "\033[36mSetting up Docker...\e[0m"
-  sudo apt install docker docker-compose -y
-
-  #adding user to docker
-  sudo usermod -aG docker $username
+  if which docker >/dev/null && which docker-compose >/dev/null; then
+    echo -e "\033[32m✔ Docker is installed\e[0m"
+  else
+    echo -e "\033[31m❌ Docker missing\e[0m"
+    echo -e "\033[36mSetting up now...\e[0m"
+    sudo apt install docker docker-compose -y
+    #adding user to docker
+    sudo usermod -aG docker $username
+  fi
 }
 
 install_python() {
-  echo -e "\033[36mSetting up Python...\e[0m"
-  sudo apt-get install python3 python3-pip -y
+  if which python3 >/dev/null; then
+    echo -e "\033[32m✔ Python3 is installed\e[0m"
+  else
+    echo -e "\033[31m❌ Python3 missing\e[0m"
+    echo -e "\033[36mInstalling now...\e[0m"
+    sudo apt-get install python3 python3-pip -y
+  fi
 }
 
 configure_zsh() {
-  echo -e "\033[36mSetting up ZSH...\e[0m"
-  sudo apt install zsh
-  #install oh-my-zsh
-  wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh
-  sed -i "s,${ZSH:-~/.oh-my-zsh},${ZSH:-/home/$username/.oh-my-zsh}," install.sh
-  chmod a+x install.sh
-  ./install.sh
-  rm install.sh
-  #zsh plugins
-  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-/home/$username/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-/home/$username/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+  if which zsh >/dev/null; then
+    echo -e "\033[32m✔ Zsh is installed\e[0m"
+  else
+    echo -e "\033[31m❌ Zsh missing\e[0m"
+    echo -e "\033[36mInstalling now...\e[0m"
+    sudo apt install zsh
+    #install oh-my-zsh
+    wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh
+    sed -i "s,${ZSH:-~/.oh-my-zsh},${ZSH:-/home/$username/.oh-my-zsh}," install.sh
+    chmod a+x install.sh
+    ./install.sh
+    rm install.sh
+    #zsh plugins
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-/home/$username/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-/home/$username/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+  fi
 
-  rm /home/$username/.zshrc
-  cd ~
-  #download .zshrc from github
-  wget https://raw.githubusercontent.com/WDaan/dotfiles/master/.zshrc -O /home/$username/.zshrc
-  sed -i "s/YOUR_USERNAME/$username/g" /home/$username/.zshrc
-  chown $username /home/$username/.zshrc
+  #restore .zshrc
+  echo -e "\033[36mrestore .zshrc?\e[0m"
+  PS3='Please enter your choice: '
+  options=("Yes" "No")
+  select opt in "${options[@]}"; do
+    case $opt in
+    "Yes")
+      rm /home/$username/.zshrc
+      cd ~
+      #download .zshrc from github
+      wget https://raw.githubusercontent.com/WDaan/dotfiles/master/.zshrc -O /home/$username/.zshrc
+      sed -i "s/YOUR_USERNAME/$username/g" /home/$username/.zshrc
+      chown $username /home/$username/.zshrc
+      echo -e "\033[36m✔ Restored .zshrc to default\e[0m"
+      break
+      ;;
+    *)
+      echo -e "\033[36mOK than...\e[0m"
+      break
+      ;;
+    esac
+  done
 }
 
 install_kubectl() {
-  echo -e "\033[36mSetting Kubectl..\e[0m"
-  kubectl version || curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
-  kubectl version || sudo chmod +x ./kubectl
-  kubectl version || sudo mv ./kubectl /usr/local/bin/kubectl
+  if which kubectl >/dev/null; then
+    echo -e "\033[32m✔ kubectl is installed\e[0m"
+  else
+    echo -e "\033[31m❌ kubectl missing\e[0m"
+    echo -e "\033[36mSetting up now..\e[0m"
+    curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
+    sudo chmod +x ./kubectl
+    sudo mv ./kubectl /usr/local/bin/kubectl
+  fi
 
-  #install brew
-  brew --version || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-
-  kubectx -h || brew install kubectx
+  if which brew >/dev/null; then
+    echo -e "\033[32m✔ brew is installed\e[0m"
+  else
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+    kubectx -h || brew install kubectx
+  fi
 }
 
 setup_composer() {
   echo -e "\033[36mSetting up PHP & Composer..\e[0m"
-  sudo apt-get install php php-mysql php-xml php-mbstring php-zip mariadb-client -y
-  sudo curl -s https://getcomposer.org/installer | php
-  sudo mv composer.phar /usr/local/bin/composer
-  composer global require laravel/installer
+  if which php >/dev/null; then
+    echo -e "\033[32m✔ php is installed\e[0m"
+  else
+    sudo apt-get install php php-mysql php-xml php-mbstring php-zip mariadb-client -y
+  fi
+
+  if which composer >/dev/null; then
+    echo -e "\033[32m✔ composer is installed\e[0m"
+  else
+    sudo curl -s https://getcomposer.org/installer | php
+    sudo mv composer.phar /usr/local/bin/composer
+    composer global require laravel/installer
+  fi
 }
 
 start_install() {
@@ -206,6 +263,7 @@ function multiselect() {
 #asking which packages
 multiselect result "update;general_packages;oh-my-zsh;node;rmate;python;docker;composer;kubectl" ";;true;true;;true;;;;;"
 
+username=$USER
 start_install
 
-echo -e "\033[32mDONE!!!🙂\e[0m"
+echo -e "\033[32m🚀 DONE!!!\e[0m"
